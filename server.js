c052ca1c3055941eb458763a7828c2d5a9ed972c
Caleb Confer <calebconfer@Calebs-MacBook-Air.local>
2021-07-01 16:44:51 -0400

    backend index, and create routes

---
+++ server.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+// dependancies
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const morgan = require('morgan');
+
+// app object
+const app = express();
+// PORT+database url acsses
+const {PORT = 5000, MONGODB_URL} = process.env;
+
+// database connection=============================
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+// connection events--
+mongoose.connection
+.on("open", () => console.log("You are connected to mongodb"))
+.on("close", () => console.log("You are disconnected from mongodb"))
+.on("error", (error) => console.log(error));
+
+// listener
+app.listen(PORT, ()=> console.log("Express is listening on port:", PORT));
+// data model====================================================
+const ChordsSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    colors: {
+        bass: Number,
+        tenor: Number,
+        alto: Number,
+        soprano: Number
+    },
+}, {timestamps: true });
+
+const Chords = mongoose.model("Chords", ChordsSchema);
+// middleware===================================
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+// routes=============================================
+// test route-----
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+// chords index route--------
+app.get('/chords', async (req, res) => {
+    try {
+        res.json(await Chords.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+// chords create route----------
+app.post("/chords", async (req, res) => {
+    try {
+        res.json(await Chords.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
